9fa988cc4e8b5cdb5b137250dbf7aa3f4da9cfcc
Ben Coleman <becolem@microsoft.com>
2019-01-27 15:56:51 +0000

    Updated client

---
+++ batclient/config.js
@@ -0,0 +1,1 @@
+var API_ENDPOINT = ""//"http://52.142.238.32:8000/api"
--- batclient/js/main.js
+++ batclient/js/main.js
@@ -1,11 +1,11 @@
-const API_ENDPOINT = "http://localhost:8000/api"
+//const API_ENDPOINT = "http://localhost:8000/api"
 var lightAnims = ["red", "green", "orange"];
 
 // Create top bank of blinking lights
 for(let y = 0; y < 3; y++) {
   for(let x = 0; x < 19; x++) {
-    let dur = parseInt((Math.random() * 900) + 100);
-    let delay = parseInt((Math.random() * 900) + 100);
+    let dur = (Math.random() * 1700) + 200;
+    let delay = Math.random() * 1500;
     var animColour = lightAnims[Math.floor(Math.random() * lightAnims.length)];
     document.getElementById('main').innerHTML += `<div class="light" style="animation-name: ${animColour}; 
       top: ${291-80+(y*28)}px; left: ${265+(x*35)}px; 
@@ -25,7 +25,13 @@ for(let y = 0; y < 3; y++) {
   }
 }
 
-function connect() {
+function connect(endpoint) {
+  endpoint = endpoint.endsWith('/') ? endpoint.substring(0, endpoint.length - 1) : endpoint
+  API_ENDPOINT = endpoint;
+
+  document.getElementById('connectEndpoint').value = "Please wait..."
+  document.getElementById('connectButton').style.visibility = "hidden"
+
   fetch(`${API_ENDPOINT}/predict/params`)
   .then(resp => resp.json())
   .then(data => {
@@ -57,6 +63,9 @@ function compute() {
     })
   }
 
+  computeStart()
+  clearPrintout()
+
   fetch(`${API_ENDPOINT}/predict`, req)
   .then(resp => resp.json())
   .then(data => {
@@ -65,6 +74,7 @@ function compute() {
     let gotAway = data.notCaughtProb * 100;
     gotAway = parseInt(gotAway);        
     printOut(`Caught: ${caught}%<br>Get Away: ${gotAway}%`)
+    computeDone()
   })
   .catch(err => {
     console.log(req);
@@ -73,15 +83,45 @@ function compute() {
   })
 }
 
-function printOut(msg, err=false) {
+function clearPrintout() {
   document.getElementById('result').innerHTML = ""
   document.getElementById('result').style.visibility = 'hidden'
   document.getElementById('result').style.animation = ''
-  document.getElementById('result').style.color = err ? 'red' : 'black'
+}
+
+function computeStart() {
+  document.getElementById('computePanel').style.visibility = 'visible'
+
+  for(let l of document.getElementsByClassName('light')) {
+    l.style.animationDuration = ((Math.random() * 150) + 20) + 'ms'
+  }
+}
+
+function computeDone() {
+  document.getElementById('computePanel').style.visibility = 'hidden'
+
+  for(let l of document.getElementsByClassName('light')) {
+    l.style.animationDuration = ((Math.random() * 1700) + 200) + 'ms';
+  }
+}
+
+
+function printOut(msg, err=false) {
+  clearPrintout()
 
   setTimeout(() => {
+    document.getElementById('result').style.color = err ? 'red' : 'black'
     document.getElementById('result').innerHTML = msg
     document.getElementById('result').style.visibility = 'visible'
     document.getElementById('result').style.animation = 'printout 2s'
   }, 50)
 }  
+
+// ENTRYPOINT - START HERE
+
+computeDone()
+
+// If endpoint set in config.js
+if(API_ENDPOINT) {
+  connect(API_ENDPOINT)
+}
