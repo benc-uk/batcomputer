8f40b68714e629c1e63507211f4bb46c290c4629
Ben Coleman <benc.uk@gmail.com>
2019-01-30 18:46:20 +0000

    Changed name of Key Vault secret scope

--- notebooks/scikit-batcomputer.py
+++ notebooks/scikit-batcomputer.py
@@ -109,36 +109,26 @@ for f, fi in ReportedByMap.items():
 
 # COMMAND ----------
 
-#
-# Widgets are how we get values passed from a DataBricks job
-#
-try:
-  #dbutils.widgets.text("model_version", "1.0.0")
-  #dbutils.widgets.text("storage_account", "modelreg")
-  #dbutils.widgets.text("model_name", "batcomputer")
+from collections import OrderedDict
+import pickle
+from azure.storage.blob import BlockBlobService
 
+try:
+  # Widgets are how we get values passed from a DataBricks job
   # Model version, name & storage-account is passed into job, and storage key is kept in Azure Key Vault
-  STORAGE_KEY       = dbutils.secrets.get("ai-deploy-secrets", "storage-key")
+  STORAGE_KEY       = dbutils.secrets.get("keyvault-secrets", "storage-key")
   STORAGE_ACCOUNT   = dbutils.widgets.get("storage_account")
   MODEL_VERSION     = dbutils.widgets.get("model_version")
   STORAGE_CONTAINER = dbutils.widgets.get("model_name")
 except:
     pass
     
-#
 # STORAGE_ACCOUNT value should only be set when this Notebook is invoked via a job
 # So we only pickle and store in Azure blobs when running as a job
-#
 if 'STORAGE_ACCOUNT' in vars():
-  print("Saving pickles to:", STORAGE_ACCOUNT, " / ", STORAGE_CONTAINER)
-  
-  # Create pickles and data lookup 
-  from collections import OrderedDict
-  import pickle
-
-  lookup = OrderedDict()
-
   # ORDER IS IMPORTANT! This is why we use OrderedDict and create entries one by one
+  # Lookup is used by the API app to convert parameter names and the string values back to encoded features
+  lookup = OrderedDict()
   lookup["force"] = ReportedByMap
   lookup["crime"] = CrimeMap
   lookup["month"] = 0
@@ -159,15 +149,12 @@ if 'STORAGE_ACCOUNT' in vars():
     pickle.dump(flags, file)    
     file.close()
 
-  from azure.storage.blob import BlockBlobService
-
   # Create the BlockBlockService that is used to call the Blob service for the storage account
   block_blob_service = BlockBlobService(account_name=STORAGE_ACCOUNT, account_key=STORAGE_KEY) 
-
   # Create a container
   block_blob_service.create_container(STORAGE_CONTAINER) 
 
-  # Upload the created file, use local_file_name for the blob name
+  # Upload the model and other pickles to the model registry
   block_blob_service.create_blob_from_path(STORAGE_CONTAINER, MODEL_VERSION + "/model.pkl", "model.pkl")
   block_blob_service.create_blob_from_path(STORAGE_CONTAINER, MODEL_VERSION + "/lookup.pkl", "lookup.pkl")
   block_blob_service.create_blob_from_path(STORAGE_CONTAINER, MODEL_VERSION + "/flags.pkl", "flags.pkl")
