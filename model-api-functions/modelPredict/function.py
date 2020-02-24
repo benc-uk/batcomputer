55c17ba12cf6a056566f6f3a2b3c745109b960aa
Ben Coleman <benc.uk@gmail.com>
2020-02-24 15:41:34 +0000

    updated function

--- model-api-functions/modelPredict/function.py
+++ model-api-functions/modelPredict/function.py
@@ -4,36 +4,39 @@ import json
 import azure.functions as func
 
 # Import for the model scoring
-#from predictor import Predictor
 from __app__.lib.predictor import Predictor
 
-pickleFolder = os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + "/../pickles")
-print('### Looking for pickles in:', pickleFolder)
-MODEL_NAME  = pickleFolder + '/model.pkl'
-LOOKUP_NAME = pickleFolder + '/lookup.pkl'
-FLAGS_NAME  = pickleFolder + '/flags.pkl'
-METADATA_NAME  = pickleFolder + '/metadata.json'
+# Location of our trained model
+modelPath = os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + "/../models")
+print('### Looking for model in:', modelPath)
+MODEL_NAME  = modelPath + '/model.pkl'
+LOOKUP_NAME = modelPath + '/lookup.pkl'
+FLAGS_NAME  = modelPath + '/flags.pkl'
+METADATA_NAME  = modelPath + '/metadata.json'
 
+# Instantiate our predictor
+predictor = Predictor(MODEL_NAME, LOOKUP_NAME, FLAGS_NAME)
+
+#
+# Function entrypoint, passed a HttpRequest object
+#
 def main(req: func.HttpRequest) -> func.HttpResponse:
   logging.info('Python HTTP trigger function processed a request.')
 
-  predictor = Predictor(MODEL_NAME, LOOKUP_NAME, FLAGS_NAME)
   try:
-    request_dict = json.loads(req.get_data().decode('utf-8'))
+    # Get input request POST data
+    request_dict = req.get_json()
+    
+    # It's all about this single line of code...
+    # ... get a prediction from the model
     results = predictor.predict(request_dict)
-    return jsonify(results)
+
+    # Pass back results as JSON
+    return json.dumps(results)
 
   except KeyError as key_error:
     print('### KEY_ERROR:', str(key_error))
-    return func.HttpResponse(json.dumps({'error': 'Value: '+str(key_error)+' not found in model lookup'}), status=400, mimetype='application/json')
+    return func.HttpResponse(json.dumps({'error': 'Value: '+str(key_error)+' not found in model lookup'}), status_code=400, mimetype='application/json')
   except Exception as err:
     print('### EXCEPTION:', str(err))
     return func.HttpResponse(json.dumps({'error': str(err)}), status_code=500, mimetype='application/json')
-
-  # if name:
-  #   return func.HttpResponse(f"Hello {name}!")
-  # else:
-  #   return func.HttpResponse(
-  #      "Please pass a name on the query string or in the request body",
-  #      status_code=400
-  #   )
